cf673bc16a4882780281fd280d56d7f9d7e96376
kaarturov <kaarturov@sberbank.ru>
2023-12-09 13:42:28 +0700

    basket card

--- fe/src/components/basket/BasketCard.tsx
+++ fe/src/components/basket/BasketCard.tsx
@@ -1,108 +1,110 @@
 import React, { FC } from 'react';
 
-import { Divider, Button, Flex, Center, Text, Card, CardBody, CardFooter } from '@chakra-ui/react';
+import { Image, Divider, Button, Flex, Stack, Text, Card, CardBody, CardFooter, Box } from '@chakra-ui/react';
 import { AddIcon, MinusIcon, DeleteIcon } from '@chakra-ui/icons';
 
 import { useBasketCard } from './hooks/useBasketCard';
 import { VNpricer } from '../../utils';
 
-import { removeBasketItem, type ShowcaseItem } from '../../store';
+import { removeBasketItem } from '../../store';
+import type { Item } from '../../shared/interfaces';
 
-const CONTROL_BUTTONS_P = 2;
 const ICONS_BOX_SIZE = 3;
 
-export interface BasketCardProps extends Partial<ShowcaseItem> {
+export interface BasketCardProps extends Partial<Item> {
   imgPath: string;
   isDisabled?: boolean;
 }
 
 export const BasketCard: FC<BasketCardProps> = (props) => {
-  const {
-    id,
-    title,
-    // type,
-    //  description,
-    price,
-    isDisabled,
-  } = props;
+  const { id, title, price, isDisabled } = props;
 
-  const { qty, incr, decr } = useBasketCard(props);
+  const { imageUrl, qty, incr, decr } = useBasketCard(props);
 
   return (
-    <Card bg='chakra-body-bg' borderRadius={8} boxShadow='inset 0 0 0 1px var(--pixpax-colors-whiteAlpha-200)'>
-      <CardBody p={2}>
-        <Flex w='full' justifyContent='space-between' alignItems='center' gap={2}>
-          <Flex w='calc(100% - 128px)' gap={2}>
-            <Button
-              isDisabled={isDisabled}
-              colorScheme='red'
-              variant='outline'
-              h='fit-content'
-              w='fit-content'
-              p={CONTROL_BUTTONS_P}
-              onClick={() => removeBasketItem(id)}
-            >
-              <DeleteIcon boxSize={ICONS_BOX_SIZE} />
-            </Button>
+    <Card w='100%' bg='chakra-body-bg' borderRadius={8} boxShadow='inset 0 0 0 1px var(--pixpax-colors-whiteAlpha-200)'>
+      <CardBody p={2} w='100%'>
+        <Flex w='100%' alignItems='flex-start' justifyContent='space-between' gap={2}>
+          <Box w='25%' aspectRatio='1 / 1' flexShrink={0}>
+            <Image
+              borderRadius={6}
+              w='full'
+              aspectRatio='1 / 1'
+              objectFit='cover'
+              src={imageUrl}
+              alt={title}
+              border='1px solid var(--pixpax-colors-telegram-200)'
+            />
+          </Box>
 
-            <Text
-              fontSize='md'
-              fontWeight='bold'
-              lineHeight='30px'
-              overflow='hidden'
-              whiteSpace='nowrap'
-              textOverflow='ellipsis'
-            >
-              {title}
+          <Stack w='calc(75% - 8px)' gap={0} flexShrink={1}>
+            <Flex gap={2} w='100%' justifyContent='space-between'>
+              <Text
+                display='block'
+                w='calc(100% - 32px - 16px)'
+                fontSize='md'
+                fontWeight='bold'
+                overflow='hidden'
+                whiteSpace='nowrap'
+                textOverflow='ellipsis'
+                color='telegram.200'
+                mb={2}
+              >
+                {title}
+              </Text>
+
+              <Button
+                isDisabled={isDisabled}
+                w='32px'
+                // colorScheme='red'
+                color='red.700'
+                opacity={0.6}
+                variant='outline'
+                size='sm'
+                onClick={() => removeBasketItem(id)}
+              >
+                <DeleteIcon boxSize={ICONS_BOX_SIZE} />
+              </Button>
+            </Flex>
+
+            <Text size='sm' color='chakra-subtle-text'>
+              Количество:
             </Text>
-          </Flex>
 
-          <Flex w='120px' justifyContent='space-between' flexShrink={0}>
-            <Button
-              flexShrink={0}
-              isDisabled={qty === 0 || isDisabled}
-              variant='outline'
-              h='fit-content'
-              w='fit-content'
-              p={CONTROL_BUTTONS_P}
-              onClick={decr}
-            >
-              <MinusIcon boxSize={ICONS_BOX_SIZE} />
-            </Button>
+            <Flex w='full' h='32px' justifyContent='space-between'>
+              <Button
+                w='64px'
+                flexShrink={0}
+                isDisabled={qty === 0 || isDisabled}
+                variant='outline'
+                size='sm'
+                onClick={decr}
+              >
+                <MinusIcon boxSize={ICONS_BOX_SIZE} />
+              </Button>
 
-            <Center flexShrink={1} w='full' h='100%'>
-              <Text fontSize='md' fontWeight='bold' lineHeight='30px'>
+              <Text flexShrink={1} w='full' fontSize='md' fontWeight='bold' textAlign='center'>
                 {qty}
               </Text>
-            </Center>
 
-            <Button
-              isDisabled={isDisabled}
-              flexShrink={0}
-              variant='outline'
-              h='fit-content'
-              w='fit-content'
-              p={CONTROL_BUTTONS_P}
-              onClick={incr}
-            >
-              <AddIcon boxSize={ICONS_BOX_SIZE} />
-            </Button>
-          </Flex>
+              <Button w='64px' isDisabled={isDisabled} flexShrink={0} variant='outline' size='sm' onClick={incr}>
+                <AddIcon boxSize={ICONS_BOX_SIZE} />
+              </Button>
+            </Flex>
+          </Stack>
         </Flex>
-      </CardBody>
 
-      {qty > 0 && (
-        <>
-          <Divider />
+        {qty > 0 && (
+          <CardFooter display='flex' flexDirection='column' p={0} mt={2}>
+            <Divider mt={2} />
 
-          <CardFooter px={2} py={0}>
             <Flex w='100%' justifyContent='space-between' fontSize='sm'>
               <Text color='chakra-subtle-text'>{`Цена: ${VNpricer.format(+price)}`}</Text>
               <Text>{`Итог: ${VNpricer.format(+price * qty)}`}</Text>
             </Flex>
           </CardFooter>
-        </>
-      )}
+        )}
+      </CardBody>
     </Card>
   );
 };
--- fe/src/components/basket/hooks/useBasketCard.ts
+++ fe/src/components/basket/hooks/useBasketCard.ts
@@ -1,15 +1,26 @@
+import { useEffect, useState } from 'react';
+import { ref as storageRef, getDownloadURL } from 'firebase/storage';
+
 import { setBasketItemQty } from '../../../store';
+import { strg } from '../../../shared/firebase';
 
 import type { BasketCardProps } from '../BasketCard';
 
 export const useBasketCard = (props: BasketCardProps) => {
-  const { id, qty } = props;
+  const { id, qty, imgPath } = props;
 
   const incr = () => setBasketItemQty({ itemId: id, qty: qty + 1 });
   const decr = () => setBasketItemQty({ itemId: id, qty: qty - 1 });
 
+  const [imageUrl, setImageURL] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    getDownloadURL(storageRef(strg, imgPath)).then((url) => setImageURL(url));
+  }, [imgPath]);
+
   return {
     qty,
+    imageUrl,
     incr,
     decr,
   };
